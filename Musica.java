7a6c5999948c124d7ef286f3bc7e93adcce95ebf
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-05-26 21:28:59 -0300

    Primeiro commit

---
+++ Musica.java
@@ -0,0 +1,9 @@
+public class Musica {
+    String titulo;
+    String artista;
+
+    public Musica(String titulo, String artista) {
+        this.titulo = titulo;
+        this.artista = artista;
+    }
+}
